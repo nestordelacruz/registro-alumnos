81222f0f87b0e9c76cceaef3ca1b4e7258c32c29
Keygurr <48705685+Keygurr@users.noreply.github.com>
2022-08-12 18:38:39 -0700

    Start

---
+++ src/Authentication.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Authentication() {
+    return (
+        <div>
+            <h1>This is the auth page</h1>
+        </div>
+    )
+}
+
+export default Authentication
