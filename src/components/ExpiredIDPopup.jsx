3827f8e6201a75ab8baad61c3ecff3e1b02f14b5
Keygurr <100051769+nestordelacruz@users.noreply.github.com>
2022-08-21 23:30:57 -0700

    Added Expired ID Popup

---
+++ src/components/ExpiredIDPopup.jsx
@@ -0,0 +1,12 @@
+import Reacct from 'react';
+
+export default function ExpiredIDPopup(props){
+    return(props.trigger) ? (
+        <div className="expired-popup"> 
+            <div className="expired-popup-inner">
+                <button className="close-btn" onClick={() => props.setTrigger(false)}>x</button>
+                { props.children }
+            </div>
+        </div>
+    ) : "";
+}   
