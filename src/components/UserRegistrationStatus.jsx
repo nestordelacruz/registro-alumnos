70f5d1d32eca3e0151398626bcf074cae427b3db
Kaladin12 <eliancruz998@gmail.com>
2022-09-05 11:39:17 -0700

    API logic done

--- src/components/UserRegistrationStatus.jsx
+++ src/components/UserRegistrationStatus.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function userRegistrationStatus(userStatus) {
-    if (userStatus == true) { 
+    if (userStatus === true) { 
 	return (
         <div className="userStatus-div">
             <p> Estudiante se encuentra registrado.</p>
